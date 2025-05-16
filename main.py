749069f8ea22ffea8b3439c7e71efebbb7b530c5
Ilia87 <chik87@mail.ru>
2025-05-16 14:03:11 +0300

    Добавил функцию для создания еды

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 import tkinter as tk
+import random
 
 
 # Настройка игр
 WIDTH = 400  # Ширина игрового поля
 HEIGHT = 400  # Высота игрового поля
+DIRECTIONS = ["Up", "Down", "Left", "Right"]
+CELL_SIZE = 10  # Размер одной клетки змейки и еды
+DELAY = 100  # Скорость игры (задержка между движениями змейки в мс)
 
 # Создание главного окна
 root = tk.Tk()
-root.title('Змейка | Счёт: 0')
+root.title("Змейка | Счёт: 0")
 root.resizable(False, False)
 
 # Создание холста для рисования
@@ -15,8 +19,8 @@ canvas = tk.Canvas(
     root,  # Родительское окно
     width=WIDTH,  # Ширина поля
     height=HEIGHT,  # Высота поля
-    bg='black',  # Цвет фона (чёрный)
-    highlightthickness=0  # Убираем границу
+    bg="black",  # Цвет фона (чёрный)
+    highlightthickness=0,  # Убираем границу
 )
 canvas.pack()  # Размещаем canvas в окне
 
@@ -26,5 +30,18 @@ direction = "Right"
 food = None
 score = 0
 game_over = False
+
+
+# Функция генерации еды
+def create_food():
+    while True:
+        x = random.randint(0, (WIDTH - CELL_SIZE) // CELL_SIZE) * CELL_SIZE
+        y = random.randint(0, (HEIGHT - CELL_SIZE) // CELL_SIZE) * CELL_SIZE
+        if (x, y) not in snake:
+            return (x, y)
+
+# Создание первой еды
+food = create_food()
+
 # Запуск главного цикла программы
 root.mainloop()
